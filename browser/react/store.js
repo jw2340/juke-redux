ad590ea3030da5941bee586c3667248cef58a599
Elliott Brooks <ms.elliottbrooks@gmail.com>
2017-02-10 11:21:41 -0500

    redux dev tools now working

--- browser/react/store.js
+++ browser/react/store.js
@@ -1,5 +1,5 @@
 import reducer from './reducers/root-reducer.js';
 import {createStore} from 'redux'; 
 
-export default createStore(reducer); 
+export default createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
 
